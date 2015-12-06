ec55edb579da7818dda580be9d400f49d6636d7e
Emily Worrall <worrall.emily@gmail.com>
2015-12-06 17:41:55 +0000

    Grid is sortable, contact section begun

--- app/assets/javascripts/mainCtrl.js
+++ app/assets/javascripts/mainCtrl.js
@@ -1,7 +1,13 @@
 app.controller('mainCtrl', function($scope, ngDialog) {
   $scope.openPopUp = function () {
-        ngDialog.open({
-          template: 'showreel'
-        });
-    };
+    ngDialog.open({
+      template: 'showreel'
+    });
+  };
+
+  $scope.openPopUp2 = function () {
+    ngDialog.open({
+      template: 'm1'
+    });
+  };
 });
