809d4c50d51449073b16e2c8b9d80d7863f77719
danielstpaul <danielstpaul@hotmail.com>
2015-12-09 16:59:40 +0000

    text overlay refactored, dialog boxes added to each existing project

--- app/assets/javascripts/mainCtrl.js
+++ app/assets/javascripts/mainCtrl.js
@@ -1,13 +1,79 @@
 app.controller('mainCtrl', ['$scope', 'ngDialog', function($scope, ngDialog) {
-  $scope.openPopUp = function () {
+  $scope.openShow = function () {
     ngDialog.open({
       template: 'showreel'
     });
   };
 
-  $scope.openPopUp2 = function () {
+  $scope.open1 = function () {
     ngDialog.open({
       template: 'm1'
+    });
+  };
+
+  $scope.open2 = function () {
+    ngDialog.open({
+      template: 'm2'
+    });
+  };
+
+  $scope.open3 = function () {
+    ngDialog.open({
+      template: 'm3'
+    });
+  };
+
+  $scope.open4 = function () {
+    ngDialog.open({
+      template: 'm4'
+    });
+  };
+
+  $scope.open5 = function () {
+    ngDialog.open({
+      template: 'm5'
+    });
+  };
+
+  $scope.open6 = function () {
+    ngDialog.open({
+      template: 'm6'
+    });
+  };
+
+  $scope.open7 = function () {
+    ngDialog.open({
+      template: 'm7'
+    });
+  };
+
+  $scope.open8 = function () {
+    ngDialog.open({
+      template: 'm8'
+    });
+  };
+
+  $scope.open9 = function () {
+    ngDialog.open({
+      template: 'm9'
+    });
+  };
+
+  $scope.open10 = function () {
+    ngDialog.open({
+      template: 'm10'
+    });
+  };
+
+  $scope.open11 = function () {
+    ngDialog.open({
+      template: 'm11'
+    });
+  };
+
+  $scope.open12 = function () {
+    ngDialog.open({
+      template: 'm12'
     });
   };
 }]);
