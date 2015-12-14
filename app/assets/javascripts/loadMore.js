1e5be7561a9e48eff1b84be10d412f00b0be13d1
danielstpaul <danielstpaul@hotmail.com>
2015-12-14 14:15:26 +0000

    testing parallax

--- app/assets/javascripts/loadMore.js
+++ app/assets/javascripts/loadMore.js
@@ -1,14 +1,7 @@
-// $(document).ready(function () {
-//   size_w3_col= $(".w3-row .w3-col").size();
-//
-//   x= 12;
-//
-//   $(".w3-row .w3-col:lt(" + x + ")").show();
-//
-//   $("#loadMore").click(function () {
-//     x= (x+4 <= size_w3_col) ? x+4  : size_w3_col;
-//
-//     $(".w3-row .w3-col:lt(" + x + ")").show();
-//
-//   });
-// });
+$(window).scroll(function () {
+
+  var fromTop = $(window).scrollTop();
+
+  $("#projects").css('margin', '-' + (100 - fromTop) + 'px 0px 0px 0px');
+
+});
