679848b2a6096c2aa85f8d1b220a042875d306bc
danielstpaul <danielstpaul@hotmail.com>
2015-12-14 12:51:20 +0000

    commented out load-more stuff

--- app/assets/javascripts/loadMore.js
+++ app/assets/javascripts/loadMore.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-  size_w3_col= $(".w3-row .w3-col").size();
-
-  x= 12;
-
-  $(".w3-row .w3-col:lt(" + x + ")").show();
-
-  $("#loadMore").click(function () {
-    x= (x+4 <= size_w3_col) ? x+4  : size_w3_col;
-
-    $(".w3-row .w3-col:lt(" + x + ")").show();
-
-  });
-});
+// $(document).ready(function () {
+//   size_w3_col= $(".w3-row .w3-col").size();
+//
+//   x= 12;
+//
+//   $(".w3-row .w3-col:lt(" + x + ")").show();
+//
+//   $("#loadMore").click(function () {
+//     x= (x+4 <= size_w3_col) ? x+4  : size_w3_col;
+//
+//     $(".w3-row .w3-col:lt(" + x + ")").show();
+//
+//   });
+// });
